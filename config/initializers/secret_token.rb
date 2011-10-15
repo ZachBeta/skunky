6fbbb9840cb3d0c2f560e6f2fcc7f81b65730bcc
Zach Morek <zmorek@gmail.com>
2011-10-15 08:54:24 -0400

    FIRST COMMIT BABY

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skunky::Application.config.secret_token = '5bf546d0022b115ff70a2435e583350c45b26508ff8e5fafd0b6c1a1352772212dae3a39fe48618f68d5f4d18c2916d13d9a672447eece969653320dab32a859'
